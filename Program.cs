8765d48a6ce017cdee3cbab7f39f252b724c0c97
VictorSardellaro <victor.henryk@outlook.com>
2021-07-15 11:07:12 -0300

    CRUD

--- Program.cs
+++ Program.cs
@@ -11,19 +11,72 @@ namespace Blog
 
         static void Main(string[] args)
         {
-            ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            DeleteUser();
         }
 
-        public static void ReadUsers()
+        public static void ReadUser()
         {
             using (var connection = new SqlConnection(CONNECTION_STRING))
             {
-                var users = connection.GetAll<User>();
+                var user = connection.Get<User>(1);
+                System.Console.WriteLine(user.Name);
 
-                foreach (var user in users)
-                {
-                    System.Console.WriteLine(user.Name);
-                }
+
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Bio = "Equipe Teste",
+                Email = "eteste@hotmail.com",
+                Image = "https://...",
+                Name = "Equipe Teste",
+                PasswordHash = "HASH",
+                Slug = "equipe-teste"
+            };
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                System.Console.WriteLine("Cadastro realizado com sucesso");
+
+
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Equipe de suporte",
+                Email = "Suporte@hotmail.com",
+                Image = "https://...",
+                Name = "Equipe de suporte",
+                PasswordHash = "HASH",
+                Slug = "equipe-suporte"
+            };
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                System.Console.WriteLine("Atualização realizada com sucesso");
+
+
+            }
+        }
+
+        public static void DeleteUser()
+        {
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+                System.Console.WriteLine("Deleção realizada com sucesso");
 
             }
         }
